77f5b49d606cd411cb81db44710f45e0f4f1ab42
Eunji0066 <nunmul0011@gmail.com>
2026-04-21 12:39:41 +0900

    초기 커밋.

---
+++ FirstGit/main.c
@@ -0,0 +1,20 @@
+#include "header.h"
+
+int main() {
+    char retry[10];
+
+    srand(time(NULL));
+
+    while (1) {
+        playgame();
+
+        printf("계속하시겠습니까 그만하시겠습니까 ");
+        scanf("%s", retry);
+
+        if (strcmp(retry, "그만") == 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
